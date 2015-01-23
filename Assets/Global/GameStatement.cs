673876af02bc96ebb3776d6802dcc166c987217a
shuitian <a8998682@163.com>
2015-01-23 13:28:31 +0800

    maze 1.06c

--- Assets/Global/GameStatement.cs
+++ Assets/Global/GameStatement.cs
@@ -6,12 +6,13 @@ public class GameStatement : MonoBehaviour {
     
     static public bool levelStatementIsDone;
     static public LevelBaseStatement levelStatement;
-    static public EnemyPool enemyPool;
-    static public BulletPool bulletPool;
+    
     static public GameStatement gameStatement;
     static public float savedTimeScale;
-
     public bool paused = false;
+
+    public EnemyPool enemyPool;
+    public BulletPool bulletPool;
 
     protected int enemiesAlive = 0;
     public bool playerIsAllAlive = true;
@@ -19,8 +20,8 @@ public class GameStatement : MonoBehaviour {
     public int gameLevel = 0;
     public int maxGameLevel = 1;
     static public bool beginGenereate;
-    static public string helpInfo = "\n  帮助信息\n  ESC:暂停\n  W:前进\tA:左移\n  S:后退\tD:右移\n  空格:跳跃\t鼠标左键:攻击\n\n  作者:puyr\n  E-mail:mazepuyr@163.com\n  版本:1.05\n";
-    static public string[] LevelTitle ={"", "荒芜平原 前章", "荒芜平原 中章", "荒芜平原 后章", "尖牙山岭", "矛石领地" , "交错之境" };
+    static public string helpInfo = "\n  帮助信息\n  ESC:暂停\n  W:前进\tA:左移\n  S:后退\tD:右移\n  空格:跳跃\t鼠标左键:攻击\n\n  作者:puyr\n  E-mail:mazepuyr@163.com\n  版本:1.06\n";
+    static public string[] LevelTitle = { "", "荒芜平原 前章", "荒芜平原 中章", "荒芜平原 后章", "尖牙山岭", "矛石领地", "交错之境 前章", "交错之境 中章" };
 
     public Mutex m = new Mutex();
     
@@ -28,8 +29,6 @@ public class GameStatement : MonoBehaviour {
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
-        bulletPool = GameObject.FindGameObjectWithTag("BulletGroup").GetComponent<BulletPool>();
-        enemyPool = GameObject.FindGameObjectWithTag("Generator").GetComponent<EnemyPool>();
     }
 
 	// Use this for initialization
--- Assets/Level/Level7Statement.cs
+++ Assets/Level/Level7Statement.cs
@@ -1,27 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Level6Statement : LevelBaseStatement
+public class Level7Statement : LevelBaseStatement
 {
     public GameObject redSphere;
     public GameObject blueSphere;
-
-    public Vector3 redPosition, bluePosition;
+    public GameObject orangeSphere;
+    public Vector3 redPosition, bluePosition, orangePosition;
     bool flag;
     
     // Use this for initialization
     protected void Awake()
     {
         base.Awake();
-        levelTitle = "交错之境 前章";
-        levelIntroduction = "\n\t当你打败矛石部队，想要离开的时候，你遇上了七彩军团之间的红蓝内战。红方与蓝方是敌人，但是你同时是他们共同的敌人。\n\t作为红龙殿的传人，你该何去何从，又该如何顺利逃脱？\n\t胜利条件:剩余敌人数小于等于0\n\t失败条件:生命值小于等于0\n";
-        info = "交错之境 前章\n\n\t胜利条件:剩余敌人数小于等于0\n\t失败条件:生命值小于等于0\n";
+        levelTitle = "交错之境 中章";
+        levelIntroduction = "\n\t你在逃脱过程中，被敌人发现，红蓝军团带来了橙色重甲兵团来消灭你。\n\t作为红龙殿的传人，你该如何击穿敌人的重甲，获取胜利？\n\t胜利条件:剩余敌人数小于等于0\n\t失败条件:生命值小于等于0\n";
+        info = "交错之境 中章\n\n\t胜利条件:剩余敌人数小于等于0\n\t失败条件:生命值小于等于0\n";
     }
 
     // Use this for initialization
     void Start()
     {
         base.Start();
+
+        flag = false;
     }
 
     // Update is called once per frame
@@ -30,7 +32,7 @@ public class Level6Statement : LevelBaseStatement
         base.Update();
         if (!flag && GameStatement.levelStatementIsDone)
         {
-            if (enemiesNumber > 300)
+            if (enemiesNumber > 400)
             {
                 flag = true;
                 return;
@@ -42,7 +44,9 @@ public class Level6Statement : LevelBaseStatement
 
                 clone = EnemyPool.Enemy(blueSphere, bluePosition, Quaternion.identity) as GameObject;
 
-                enemiesNumber += 2;
+                clone = EnemyPool.Enemy(orangeSphere, orangePosition, Quaternion.identity) as GameObject;
+
+                enemiesNumber += 3;
                 GameStatement.beginGenereate = true;
             }
         }
